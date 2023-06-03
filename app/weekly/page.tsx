4c84421db13c5ce91e1f5ed4834960e294f9622b
Nurçin <nrcinozer@gmail.com>
2023-06-03 11:26:01 +0300

    feat: new weekly post added

--- app/weekly/page.tsx
+++ app/weekly/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import Link from 'next/link';
 import { allBlogs } from 'contentlayer/generated';
 import ViewCounter from './view-counter';
+import { ArrowIcon } from 'components/icons';
 
 export const metadata: Metadata = {
 	title: 'Blog',
@@ -11,7 +12,13 @@ export const metadata: Metadata = {
 export default async function BlogPage() {
 	return (
 		<section>
-			<h1 className="font-bold text-3xl font-serif mb-5">Blog</h1>
+			<h1 className="font-bold text-3xl font-serif">Weekly</h1>
+			<p className="my-5 text-neutral-800 dark:text-neutral-200">
+				Every week I write a short post about what I've been reading,
+				watching, and learning. I also share some of my favorite links
+				from around the web.
+			</p>
+			<hr className="my-8 border-neutral-200 dark:border-neutral-700" />
 			{allBlogs
 				.sort((a, b) => {
 					if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
@@ -22,11 +29,11 @@ export default async function BlogPage() {
 				.map(post => (
 					<Link
 						key={post.slug}
-						className="flex flex-col space-y-1 mb-4"
+						className="flex space-y-1 mb-4"
 						href={`/weekly/${post.slug}`}>
-						<div className="w-full flex flex-col">
+						<div className="w-full flex items-center">
+							<ArrowIcon />
 							<p>{post.title}</p>
-							<ViewCounter slug={post.slug} trackView={false} />
 						</div>
 					</Link>
 				))}
