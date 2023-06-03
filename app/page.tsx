aea17cf8530d0738bb9d9d88e4dd3e3218564005
Nurçin <nrcinozer@gmail.com>
2023-06-04 00:14:56 +0300

    style: width of home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,20 @@ export default async function HomePage() {
 	return (
 		<section>
 			<h1 className="font-bold text-3xl font-serif">{name}</h1>
-			<div className="flex items-start my-8 flex-col md:flex-row gap-4">
+			<div className="flex items-start my-8 flex-col md:flex-row gap-6">
 				<Image
 					alt={name}
 					className="rounded-lg grayscale"
 					src={avatar}
 					placeholder="blur"
-					width={100}
+					width={110}
 					priority
 				/>
-				<div className="max-w-[480px]">
+				<div className="w-full">
 					<p className="text-neutral-800 dark:text-neutral-200">
 						{about()}
 					</p>
-					<p className="mt-5 text-neutral-800 dark:text-neutral-200">
+					<p className="mt-4 text-neutral-800 dark:text-neutral-200">
 						{bio()}
 					</p>
 				</div>
