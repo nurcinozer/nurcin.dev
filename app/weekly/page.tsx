4d52ccaacc98fa72a857b374e1bf579082b4fa0c
Nurçin <nrcinozer@gmail.com>
2023-07-02 01:56:12 +0300

    feat: newsletter subscription added

--- app/weekly/page.tsx
+++ app/weekly/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import Link from 'next/link';
 import { allBlogs } from 'contentlayer/generated';
 import { ArrowIcon } from 'components/icons';
+import { useRef, useState } from 'react';
+import Subscription from 'components/subscription';
 
 export const metadata: Metadata = {
 	title: 'Weekly',
@@ -11,32 +13,43 @@ export const metadata: Metadata = {
 
 export default async function BlogPage() {
 	return (
-		<section className="border border-neutral-200 dark:border-neutral-800 rounded-lg md:p-8 p-4">
-			<h1 className="font-bold text-3xl font-serif">Weekly</h1>
-			<p className="my-5 text-neutral-800 dark:text-neutral-200">
-				I am sharing a short post on what I’ve been reading, watching,
-				and learning as well as a few favourite links from the web every
-				week.
-			</p>
-			<hr className="my-8 border-neutral-200 dark:border-neutral-700" />
-			{allBlogs
-				.sort((a, b) => {
-					if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-						return -1;
-					}
-					return 1;
-				})
-				.map(post => (
-					<Link
-						key={post.slug}
-						className="flex space-y-1 mb-4"
-						href={`/weekly/${post.slug}`}>
-						<div className="w-full flex items-center">
-							<ArrowIcon />
-							<p>{post.title}</p>
-						</div>
-					</Link>
-				))}
-		</section>
+		<>
+			<section className="border border-neutral-200 dark:border-neutral-800 rounded-lg md:p-8 p-4">
+				<h1 className="font-bold text-3xl font-serif">Weekly</h1>
+				<p className="my-5 text-neutral-800 dark:text-neutral-200">
+					I am sharing a short post on what I’ve been reading,
+					watching, and learning as well as a few favourite links from
+					the web every week.
+				</p>
+				<hr className="my-8 border-neutral-200 dark:border-neutral-700" />
+				{allBlogs
+					.sort((a, b) => {
+						if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
+							return -1;
+						}
+						return 1;
+					})
+					.map(post => (
+						<Link
+							key={post.slug}
+							className="flex space-y-1 mb-4"
+							href={`/weekly/${post.slug}`}>
+							<div className="w-full flex items-center">
+								<ArrowIcon />
+								<p>{post.title}</p>
+							</div>
+						</Link>
+					))}
+			</section>
+			<section className="mt-8 border border-neutral-200 dark:border-neutral-800 rounded-lg md:p-8 p-4">
+				<h1 className="font-bold text-3xl font-serif">Subscribe</h1>
+				<p className="my-5 text-neutral-800 dark:text-neutral-200">
+					Subscribe to my newsletter to get notified when I publish a
+					new post.
+				</p>
+				<hr className="my-8 border-neutral-200 dark:border-neutral-700" />
+				<Subscription />
+			</section>
+		</>
 	);
 }
---
+++ components/subscription.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { FormEvent, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+export default function Subscription() {
+	const [emailInput, setEmailInput] = useState('');
+
+	const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (!emailInput) {
+			return toast('Please enter your email.', {
+				icon: '❌',
+				style: {
+					borderRadius: '10px',
+					background: '#333',
+					color: '#fff',
+				},
+				position: 'bottom-center',
+			});
+		}
+		try {
+			const res = await fetch('/api/subscribe', {
+				method: 'POST',
+				body: JSON.stringify({ email: emailInput }),
+			});
+			const data = await res.json();
+			if (data.success) {
+				toast('Thanks for subscribing!', {
+					icon: '👏',
+					style: {
+						borderRadius: '10px',
+						background: '#333',
+						color: '#fff',
+					},
+					position: 'bottom-center',
+				});
+			} else {
+				throw new Error(
+					data?.error ||
+						'Something went wrong, please try again later',
+				);
+			}
+		} catch (e) {
+			toast((e as Error).message, {
+				icon: '❌',
+				style: {
+					borderRadius: '10px',
+					background: '#333',
+					color: '#fff',
+				},
+				position: 'bottom-center',
+			});
+		}
+		setEmailInput('');
+	};
+
+	return (
+		<form onSubmit={handleFormSubmit}>
+			<div className="flex flex-col sm:flex-row gap-2 justify-center">
+				<input
+					value={emailInput}
+					onChange={e => setEmailInput(e.target.value)}
+					type="email"
+					placeholder="Your email"
+					className="border border-neutral-800 bg-transparent rounded-lg px-4 py-2 md:w-2/3 w-full outline-none placeholder-neutral-200"
+					required
+				/>
+				<button
+					type="submit"
+					className="border border-neutral-200 dark:border-neutral-800 rounded-lg px-4 py-2 md:w-1/3 w-auto">
+					Subscribe
+				</button>
+			</div>
+		</form>
+	);
+}
